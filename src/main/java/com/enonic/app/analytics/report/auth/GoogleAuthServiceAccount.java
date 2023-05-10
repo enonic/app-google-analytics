e8b6b14f04c0f2edf48fa48e9f73133c3396066a
Persijn Kwekkeboom <pkw@enonic.com>
2023-05-10 17:31:42 +0200

    Working api call to report

--- src/main/java/com/enonic/app/analytics/report/auth/GoogleAuthServiceAccount.java
+++ src/main/java/com/enonic/app/analytics/report/auth/GoogleAuthServiceAccount.java
@@ -3,71 +3,144 @@ package com.enonic.app.analytics.report.auth;
 import com.enonic.xp.script.bean.BeanContext;
 import com.enonic.xp.script.bean.ScriptBean;
 
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.services.analyticsdata.v1beta.AnalyticsData;
-import com.google.api.services.analyticsdata.v1beta.AnalyticsDataRequest;
-import com.google.auth.oauth2.AccessToken;
+import com.google.analytics.data.v1beta.*;
+import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.gson.Gson;
-import org.apache.commons.io.FileExistsException;
+import com.google.gson.GsonBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class GoogleAuthServiceAccount implements ScriptBean {
     private static final Logger log  = LoggerFactory.getLogger(GoogleAuthServiceAccount.class);
+
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    private static final String APPLICATION_NAME = "Enonic google Analytics data widget";
     public void initialize(BeanContext context) {
 
     }
 
-    public String authenticate(String credentialPath) throws Exception {
-        Gson gson = new Gson();
-
-        if (credentialPath == null) {
+    private BetaAnalyticsDataClient initializeAnalytics(String credentialPath) throws Exception {
+        if (credentialPath == null || credentialPath.isEmpty()) {
             log.warn("Missing path to credentials file in app configuration");
+            throw new Exception("No credentialsFile provided");
         }
 
         Path credentialFile = Paths.get(credentialPath);
-        GoogleCredentials credentials = null;
+        GoogleCredentials credentials = ServiceAccountCredentials.fromStream(
+                Files.newInputStream(credentialFile)
+        );
+
+        BetaAnalyticsDataSettings clientSettings = BetaAnalyticsDataSettings.newBuilder()
+                .setCredentialsProvider(FixedCredentialsProvider.create(credentials))
+                .build();
+
+        BetaAnalyticsDataClient analyticsData;
         try {
-            credentials = GoogleCredentials.fromStream(
-                    Files.newInputStream(credentialFile)
-            );
-        } catch (FileExistsException e) {
-            log.error("File exception " + e.getMessage());
-        } catch (Exception e) {
-            log.error("Authentication error " + e.getMessage());
+             analyticsData = BetaAnalyticsDataClient.create(clientSettings);
+        } catch (IOException exception) {
+            throw new UncheckedIOException(exception);
         }
 
-        credentials.refreshIfExpired();
-        AccessToken token = credentials.getAccessToken();
-        AnalyticsData data = new AnalyticsData(
-                GoogleNetHttpTransport.newTrustedTransport(),
-                new GsonFactory(),
-                request -> {
-                    log.info("Request callback?");
-                    log.info(request.getContent().toString());
-                }
-        );
-        AnalyticsDataRequest widgetRequest = new AnalyticsDataRequest(
-                data,
-                "GET", //todo posting later
-                
-
-        );
-        data.
-
-        /*(
-                GoogleNetHttpTransport.newTrustedTransport(),
-                new GsonFactory(),
-        );*/
-
-        return "credentials created";
+        return analyticsData;
     }
 
+    public String authenticate(String analyticsCode, String credentialPath) throws Exception {
+        //TODO check analyticsCode and credentialPath for empty, null, missing
 
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+//        String currentDate = formatter.format(new Date());
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.YEAR, -1);
+        String previousYear = formatter.format(cal.getTime());
+
+        RunReportRequest request =
+                RunReportRequest.newBuilder()
+                        .setProperty("properties/" + analyticsCode)
+                        .addDimensions(Dimension.newBuilder().setName("country"))
+                        .addDimensions(Dimension.newBuilder().setName("date"))
+                        .addMetrics(Metric.newBuilder().setName("activeUsers"))
+                        .addDateRanges(
+                                DateRange.newBuilder()
+                                        .setStartDate(previousYear)
+                                        .setEndDate("today")
+                        )
+                        .build();
+
+        final BetaAnalyticsDataClient analyticsData = initializeAnalytics(credentialPath);
+        PrettyReportData reportOut;
+        try (analyticsData) {
+            RunReportResponse response = analyticsData.runReport(request);
+
+            reportOut = new PrettyReportData();
+
+            for (DimensionHeader diHead : response.getDimensionHeadersList()) {
+                reportOut.addDimensionHeader(diHead.getName());
+            }
+            for (MetricHeader metHead : response.getMetricHeadersList()) {
+                reportOut.addMetricHeader(metHead.getName());
+            }
+            for (Row row : response.getRowsList()) {
+                RowObject rowValues = new RowObject();
+                for (int i = 0; i>row.getDimensionValuesCount(); i++) {
+                    rowValues.dimensionValues.add(
+                            new ReportValue(row.getDimensionValues(i).getValue())
+                    );
+                }
+                for (int j = 0; j>row.getMetricValuesCount(); j++) {
+                    rowValues.metricValues.add(
+                            new ReportValue(row.getDimensionValues(j).getValue())
+                    );
+                }
+
+                reportOut.reportRow.add(rowValues);
+            }
+        }
+
+        log.info(gson.toJson(reportOut));
+
+        return ":D";
+    }
+
+    class PrettyReportData {
+        private ArrayList<String> dimentions = new ArrayList<String>();
+        private ArrayList<String> metrics = new ArrayList<String>();
+        private ArrayList<RowObject> reportRow = new ArrayList<RowObject>();
+
+        public void addMetricHeader(String header) {
+            metrics.add(header);
+        }
+
+        public void addDimensionHeader(String header) {
+            dimentions.add(header);
+        }
+
+        public void addRow(RowObject nextRow) {
+            reportRow.add(nextRow);
+        }
+    }
+
+    class ReportValue {
+        private String value;
+        ReportValue(String value) {
+            this.value = value;
+        }
+    }
+
+    class RowObject {
+        public ArrayList<ReportValue> dimensionValues = new ArrayList<ReportValue>();
+        public ArrayList<ReportValue> metricValues = new ArrayList<ReportValue>();
+    }
 }
--- src/main/resources/admin/widgets/analytics-report/analytics-report.js
+++ src/main/resources/admin/widgets/analytics-report/analytics-report.js
@@ -1,16 +1,63 @@
 const portalLib = require('/lib/xp/portal');
+const contentLib = require('/lib/xp/content');
 const thymeleaf = require('/lib/thymeleaf');
 
 function get(req) {
     const ga_auth = __.newBean("com.enonic.app.analytics.report.auth.GoogleAuthServiceAccount");
+    let credentialPath = "";
 
-    // ga_auth.authenticate();
+    if (app.config) {
+        //TODO document key name + example
+        if (app.config && app.config["ga.credentialPath"]) {
+            // log.info(`CredentialPath: ${app.config["ga.credentialPath"]}`);
+            credentialPath = app.config["ga.credentialPath"];
+        } else {
+            return ErrorResponse("No path to credentials found");
+        }
+    }
+
+    let contentId = req.params.contentId ? req.params.contentId : portalLib.getContent()._id;
+
+    if (!contentId) {
+        return ErrorResponse('No content selected');
+    }
+
+    let siteConfig;
+
+    // This is one annoying way to get the siteConfig.
+    // Second alternative is executing it in a context also a lot of code.
+    const site = contentLib.getSite({key: contentId});
+    if (site.data && site.data.siteConfig) {
+        site.data.siteConfig.forEach(element => {
+            if (element.applicationKey = app.name) {
+                siteConfig = element.config;
+            }
+        });
+
+    }
+    if (!siteConfig) {
+        //TODO Trigger this aka test it
+        return ErrorResponse("Missing measure id? No site configuration found for application.");
+    }
+
+    log.info(`Measure id ${siteConfig.measureId}`);
+
+    const a = ga_auth.authenticate(siteConfig.measureId, credentialPath);
+
+    log.info(JSON.stringify(a));
 
     const widgetId = app.name;
 
     return {
         contentType: 'text/html',
         body: `<widget id="widget-${widgetId}">:D</widget>`,
+    }
+}
+
+function ErrorResponse(message) {
+    return {
+        contentType: 'text/html',
+        body: `<widget class="error">${message}</widget>`,
     }
 }
 
